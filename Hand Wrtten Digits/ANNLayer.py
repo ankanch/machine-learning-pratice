82ce36f5cc9d1b3f49ac10f32ee27bfccb8e308c
Kanch <kanchisme@gmail.com>
2017-07-04 17:41:01 +0800

    UPDATE FORWARD PROPAGATION

--- Hand Wrtten Digits/ANNLayer.py
+++ Hand Wrtten Digits/ANNLayer.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 import activiation
 
 class ANNLayer:
@@ -16,10 +17,15 @@ class ANNLayer:
     var_layer_name = "defualt"
     var_neurons = [0,0]
 
-    def __init__(self,neuron_amount,bias=0,name="default"):
+    def __init__(self,neuron_amount,bias=0,name="default",value=[]):
         self.var_neuron_amount = neuron_amount
         self.var_bias_value = bias
         self.var_layer_name = name
+        if len(value) == neuron_amount:
+            self.var_neurons = value.copy()
+
+    def getNeuronValues(self):
+        return self.var_neurons.copy()
     
     def setNeuron(self,amount,values=[]):
         """
@@ -35,7 +41,7 @@ class ANNLayer:
 
     def setBias(self,value=0):
         """
-        Set the bias unit value to current layer
+        Set the bias unit value to current layer.
         If `0` is assigned, it will be treated as no bias unit.
         """
         self.var_bias_value = value
@@ -43,7 +49,8 @@ class ANNLayer:
     def forward_propagation(self,theta):
         """
         This function is used to perfom forward propagation with given weight for current layer.
-        This will return the input to the next layer, a list.`theta` can be a  2D list
+        This will return the input to the next layer, a list.
+        * `theta` need to be a  2D list
         """
         # just convert var_neurons to a column vector by using np.matrix
         inputVc = np.matrix(self.var_neurons)
@@ -63,6 +70,30 @@ class ANNLayer:
         print("after apply sigmoid function:\n",hiv2)
 
         return hiv2
+
+    def randomInitTheta(self,roundv=0):
+        """
+        This function will random initialize a theta matrix for current layer.
+        This usually used in the first run of this network.
+        This function will return a matrix depend on the size of this layer.
+        """
+        ssize = self.var_neuron_amount
+        thetax = [0] * ssize
+        thetax = [thetax]*ssize
+        
+        #then we initialize these value into [0,1] 
+        i=0
+        j=0
+        for vector in thetax:
+            for value in vector:
+                if roundv == 0:
+                    thetax[i][j] = random.uniform(0, 1)
+                else:
+                    thetax[i][j] = round(random.uniform(0, 1),roundv)
+                j+=1
+            i+=1
+            j=0
+        return thetax
         
 
     def printLayerInfo(self):
@@ -83,4 +114,26 @@ if __name__ == "__main__":
     la.setBias(0)
     la.printLayerInfo()
     la.forward_propagation([[1,2,3,4],[1,2,3,4],[4,5,6,7],[9,1,0,2]])
+    # example neural network with 1 input, 1 hidden and 1 output
+    # network size: 3*3*2
+    print("tset for a example neural network with 1 input layer,1 hidden and 1 output.(3x3x2)")
+    iv = [10,20,5]
+    la_input = ANNLayer(3,0,"input",iv)
+    la_hidden = ANNLayer(3,0,"hidden 1")
+    la_output = ANNLayer(2,0,"output")
+    print("input list is:\n",iv)
+    theta_12 = la_input.randomInitTheta(2)
+    theta_23 = la_hidden.randomInitTheta(2)
+    print("random initialized theta matrix from layer 1 to 2:\n",theta_12)
+    print("random initialized theta matrix from layer 2 to 3:\n",theta_23)
+    print("forward propagation from layer 1 to layer 2:\n")
+    la_hidden.setNeuron(la_hidden.var_neuron_amount,la_input.forward_propagation(theta_12))
+    la_hidden.printLayerInfo()
+    print("forward propagation from layer 2 to layer 3:\n")
+    la_output.setNeuron(la_output.var_neuron_amount,la_hidden.forward_propagation(theta_23))
+    la_output.printLayerInfo()
+    print("neural network output list is:\n")
+
+    
+
     
--- Hand Wrtten Digits/learning.py
+++ Hand Wrtten Digits/learning.py
@@ -1,11 +1,44 @@
-
+from ANNLayer import ANNLayer
 
 def backpropagation(output,weight):
     pass
 
-def forwardpropagation(input,weight):
-    pass
-
+def forwardpropagation(layers,weights,inputs=[]):
+    """
+    This function takes a list of layers and a list of theta matrix.
+    Then this function will run forwad propagation to it.Then return the output vector    
+    * `inputs`: the raw input(single list) to the first layer,if not specified, assume input is already set in layer 1
+    * `layers`: the layer list that contains input, hidden and output layers, at least two layers
+    * `weights`: list of weight (theta) matrix
+    """
+    first = True
+    weight_pos = 0
+    layer_pos = 1
+    for layer in layers:
+        if first:
+            if len(inputs) == layer.var_neuron_amount:
+                layer.setNeuron(layer.var_neuron_amount,inputs)
+            layers[layer_pos].setNeuron(layers[layer_pos].var_neuron_amount,layer.forward_propagation(weights[weight_pos]))
+            first = False
+        else:
+            layers[layer_pos].setNeuron(layers[layer_pos].var_neuron_amount,layer.forward_propagation(weights[weight_pos]))
+        weight_pos+=1
+        layer_pos+=1
+        if layer_pos == len(layers):
+            break
+    return layers[len(layers)-1].getNeuronValues()
 
 if __name__ == "__main__":
-    print("test for learning:")
+    print("test for learning:\ncreating network")
+    iv = [10,20,5]
+    la_input = ANNLayer(3,0,"input",iv)
+    la_hidden = ANNLayer(3,0,"hidden 1")
+    la_output = ANNLayer(2,0,"output")
+    las = [ la_input,la_hidden,la_output ]
+    theta_12 = la_input.randomInitTheta(2)
+    theta_23 = la_hidden.randomInitTheta(2)
+    thetas = [ theta_12 , theta_23 ]
+    print("start perform forward propagation.\n")
+    result = forwardpropagation(las,thetas,iv)
+    print("result vector:",result)
+    las[len(las)-1].printLayerInfo()
