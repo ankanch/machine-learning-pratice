837a4fbaf82492bbea2bea02c0923b4e68822b5b
Kanch J <kanchisme@gmail.com>
2017-09-04 23:05:18 +0800

    Add Digital Recogniziation done under tutorial of Kaggle.

---
+++ Kaggle - Digit Recognizer/SVM/predict.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from sklearn import svm
+from sklearn.externals import joblib
+
+PATH_TEST = "../data/test.csv"
+
+# load model
+print(">>>loading model...")
+clf = joblib.load('svm.model') 
+
+# load test data and predict
+print(">>>loading data...")
+test_data=pd.read_csv(PATH_TEST)
+test_data[test_data>0]=1
+
+print(">>>predicting...")
+results=clf.predict(test_data)
+
+# output result
+print(">>>saving results...")
+df = pd.DataFrame(results)
+df.index.name='ImageId'
+df.index+=1
+df.columns=['Label']
+df.to_csv('results.csv', header=True)
+print(">>>done.")
---
+++ Kaggle - Digit Recognizer/SVM/train.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt, matplotlib.image as mpimg
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn.externals import joblib
+import disp_multiple_images
+
+PATH_TRAIN = "../data/train.csv"
+
+# load training data
+print(">>>loading data...")
+labeled_images = pd.read_csv(PATH_TRAIN)
+images = labeled_images.iloc[:,1:]
+labels = labeled_images.iloc[:,:1]
+train_images, test_images,train_labels, test_labels = train_test_split(images, labels, train_size=0.8, random_state=0)
+
+# make picture from grayscale to turely black white image
+print(">>>transforming data...")
+test_images[test_images>0]=1        # set all pixels that noezero to 1
+train_images[train_images>0]=1      # zero stays the same.
+img=train_images.as_matrix()
+
+# draw picture
+if False:
+    print("before reshape all image to 28x28:",img.shape)
+    print("one of the image size is:",len(img[0]))
+    img =  np.asarray([ i.reshape(28,28) for i in img])
+    print("after reshape all image to 28x28:",img.shape)
+    disp_multiple_images.show_images(img[:16],4)
+
+# train SVM
+print(">>>start training model...")
+clf = svm.SVC()
+clf.fit(train_images, train_labels.values.ravel())
+print(">>>Model training has been finished.Running test for current model...")
+print(">>>Test Score:",clf.score(test_images,test_labels),"\n>>>saving model...")
+joblib.dump(clf, 'svm.model') 
+print(">>>model saved.\n>>>done.")
+
+
