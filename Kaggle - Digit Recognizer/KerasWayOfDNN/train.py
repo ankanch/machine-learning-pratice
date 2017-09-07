bcd78c28ad173b92c417f178d873d941250877d7
Kanch J <kanchisme@gmail.com>
2017-09-07 15:44:08 +0800

    Add print training history fingure.

--- Kaggle - Digit Recognizer/KerasWayOfDNN/train.py
+++ Kaggle - Digit Recognizer/KerasWayOfDNN/train.py
@@ -1,7 +1,9 @@
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 from keras.models import Sequential
-from keras.layers import Dense,Flatten
+from keras.layers import Dense,Flatten,Dropout
+
+from printTrainingHistory import plotLossAndAcc
 pd.options.mode.chained_assignment = None
 
 PATH_TRAIN = "../data/train.csv"
@@ -12,7 +14,8 @@ labeled_images = pd.read_csv(PATH_TRAIN)
 images = labeled_images.iloc[:,1:]
 labels = labeled_images.iloc[:,:1]
 print(">>>preprocessing data...")
-images[images>0]=1
+#images[images>0]=1
+images /= 255
 images = images.as_matrix().reshape(images.shape[0], 28, 28,1)
 pm = []
 for x  in labels.as_matrix():
@@ -25,19 +28,23 @@ print(">>>images.shape=",images.shape,"\tlabels.shape=",labels.shape)
 # setup an ANN
 print('>>>setup sequential nerual network...')
 model = Sequential()
-model.add(Dense(units=32,activation='hard_sigmoid',input_shape=(28,28,1) ))
+model.add(Dense(units=64,activation='relu',input_shape=(28,28,1) ))
 model.add(Flatten())
-model.add(Dense(units=10,activation='hard_sigmoid'))
+model.add(Dense(units=10,activation='softmax'))
 print('>>>model.input_shape=',model.input_shape,"\tmodel.output_shape=",model.output_shape)
 print('>>>compiling...')
-model.compile(optimizer='sgd',loss='mean_squared_error',metrics=['accuracy'])
+model.compile(optimizer="adam",loss='mean_squared_error',metrics=['accuracy'])
 
 # start training this model
 print('>>>training model...')
-model.fit(images,labels,epochs=40,verbose=1,validation_split=0.2)
+history = model.fit(images,labels,epochs=15,verbose=1,validation_split=0.1,batch_size=128)
 
 # save model
 print('>>>done.\n>>>saving model...')
 model.save('digital_recog_w_sequentialDenseNN.h5fmodel')  # creates a HDF5 file 'my_model.h5'
+
+# plot training history
+plotLossAndAcc(history)
 print(">>>all done.")
+
 
